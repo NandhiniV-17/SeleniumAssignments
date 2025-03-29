3bd34302537144aa3821a2f302c7f1db6f86de2e
NandhiniV-17 <117360180+NandhiniV-17@users.noreply.github.com>
2025-03-29 21:27:22 +0530

    Add files via upload
    Day 1 Assignment

---
+++ day1/Chrome.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		float version = 91.0f;
+		String developer = "Google";
+		boolean isBeta = false;
+		int releaseYear = 2008;
+		char shortcutKey = 'C';
+		System.out.println(
+				"The float value is " + version + "\nThe string value is " + developer + "\nThe boolean value is "
+						+ isBeta + "\nThe int value is " + releaseYear + "\nThe char value is " + shortcutKey);
+
+	}
+
+}
